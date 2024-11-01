e816d2a22a19412a3f25aa4cc7eaa8d409982f4d
mcnultye <157040192+mcnultye@users.noreply.github.com>
2024-11-01 13:15:08 -0400

    create game logic but cards are appearing

--- components/deck.js
+++ components/deck.js
@@ -20,9 +20,9 @@ class Deck{
             var card = {
                 suit: suit,
                 value: val,
-                obj: createCardObject(createTextures(suit,val))
+                obj: this.createCardObject(this.createTextures(suit,val))
             }
-            deck.push(card);
+            this.deck.push(card);
         });
    });
 }
@@ -31,35 +31,53 @@ class Deck{
  createTextures(suit,val){
     const loader = new THREE.TextureLoader();
     if(val <= 10){
-        return loader.load('cardT/' + val + suit + '.png');
+        return loader.load('components/cardT/' + val + suit + '.png');
     } else if (val == 11){
-        return loader.load('cardT/jack' + suit + '.png');
+        return loader.load('components/cardT/jack' + suit + '.png');
     } else if (val == 12){
-        return loader.load('cardT/queen' + suit + '.png');
+        return loader.load('components/cardT/queen' + suit + '.png');
     } else if (val == 13){
-        return loader.load('cardT/king' + suit + '.png');
+        return loader.load('components/cardT/king' + suit + '.png');
     } else if (val == 14){
-        return loader.load('cardT/ace' + suit + '.png');
+        return loader.load('components/cardT/ace' + suit + '.png');
     }
 }
 
 //creating the card object
- createCardObject(texture){
-    const geometry = new THREE.BoxGeometry(.4,.6,.001);
-    const materials = [
-        new THREE.MeshBasicMaterial(),
-        new THREE.MeshBasicMaterial(),
-        new THREE.MeshBasicMaterial(),
-        new THREE.MeshBasicMaterial(),
-        new THREE.MeshBasicMaterial({map: 'cardT/'+texture}),
-        new THREE.MeshBasicMaterial({map: 'cardT/back.png'})
+createCardObject(texture) {
+    var geometry = new THREE.BoxGeometry(.4, .6, .001);
+
+    // Define materials for each face of the card
+    var frontMaterial = new THREE.MeshBasicMaterial({ map: texture }); // Card front with custom texture
+    var backMaterial = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('components/cardT/back.png') }); // Card back texture
+    var sideMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 }); // Sides, simple black color
+
+    var materials = [
+        sideMaterial, // right
+        sideMaterial, // left
+        sideMaterial, // top
+        sideMaterial, // bottom
+        frontMaterial, // front
+        backMaterial  // back
     ];
-    const mesh = new THREE.Mesh(geometry, materials);
+
+    var mesh = new THREE.Mesh(geometry, materials);
+    mesh.castShadow = true;
+    mesh.receiveShadow = true;
     return mesh;
 }
 
-    getDeck() {
-        return this.deck;
+    shuffle() {
+        for (let i = this.deck.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.deck[i], this.deck[j]] = [this.deck[j], this.deck[i]];
+        }
+    }
+
+    deal(playersCount) {
+        const piles = Array.from({ length: playersCount }, () => []);
+        this.deck.forEach((card, index) => piles[index % playersCount].push(card));
+        return piles;
     }
 }
 
